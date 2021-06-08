accbf24634594caab7cc59ecedb23c7fd7e8594d
SupitS <84420067+SupitS@users.noreply.github.com>
2021-06-08 14:27:24 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+// Opdracht 014: Gebruik if/else statements
+
+console.log('Hallo Winc Academy studenten');
+
+const age = 18;
+var number = 18;
+if (number >= 18) {
+    console.log('Welkom, kom binnen! ');
+} else {
+    console.log('Gezien uw leeftijd mogen wij u helaas niet binnenlaten.');
+}
+
+const isFemale = true;
+if (isFemale) {
+    console.log('You are a female');
+} else {
+    console.log('You are not a female');
+}
+
+const driverStatus = 'David';
+if (driverStatus == 'Bob') {
+    console.log('Jij bent BOB. ');
+} else {
+    console.log('Jij bent niet BOB en mag niet rijden.');
+}
